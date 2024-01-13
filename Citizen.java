d672ea754bbb318be84b46f08a5c2e2fcfdf75d6
think0507 <think0507@naver.com>
2024-01-13 23:58:28 +0900

    출력값 수정/ 시민 생성 실패

--- Citizen.java
+++ Citizen.java
@@ -13,6 +13,6 @@ public class Citizen {
 
     @Override
     public String toString(){
-        return "이름은"+name+"국적은"+nationality+" 입니다";
+        return "이름은"+name+", 국적은 "+nationality+", 위치는 "+whereIAm+"입니다"+"\n-------------------------------------";
     }
 }
